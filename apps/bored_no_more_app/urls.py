790440dc6f773c735316facc78fe7e7ce8ff6d15
Jacob Gronlund <jacobgronlund@Jacobs-MacBook-Pro-2.local>
2017-07-27 23:10:12 -0700

    bootstrapped version of index for events

--- apps/bored_no_more_app/urls.py
+++ apps/bored_no_more_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'getcategory$', views.getcategory)
+    url(r'getcategory$', views.getcategory),
+    url(r'getpage$', views.getpage)
 ]
--- apps/bored_no_more_app/views.py
+++ apps/bored_no_more_app/views.py
@@ -5,15 +5,19 @@ import requests
 from eventbrite import Eventbrite
 # HTML-based index method
 def index(request):
+    
     return render(request, 'bored_no_more_app/index.html')
 def getcategory(request):
     category = request.POST['category']
     location = request.POST['location'].replace(" ","+")
+    # request.session['category'] = category
+    # request.session['location'] = location
     print category
     print location
     #category = "movies_film"
     #url = "http://eventful.com/oauth/authorize?oauth_token=6ea35d914da91eec8c00"
-    url = "http://api.eventful.com/json/events/search?app_key=29hhnm59QqVdfQWf&category=" +category+ "&location=" +location+ "&date=Future&page_size=20"
+    url = "http://api.eventful.com/json/events/search?app_key=29hhnm59QqVdfQWf&category=" +category+ "&location=" +location+ "&date=ThisWeek&page_size=100"
+    #url = "http://api.eventful.com/json/events/search?app_key=29hhnm59QqVdfQWf&date=ThisWeek&Location=Florida&page_size=50&"
     #url = "http://api.eventful.com/json/events/search?app_key=29hhnm59QqVdfQWf&category=comedy&location=San+Diego&date=Future&page_size=20"
     # url = "https://www.eventbriteapi.com/v3/events/?token=W5AC5ZM4E7DU3FZLEWHH"
     # notice this is 'requests' not 'request'
@@ -24,3 +28,9 @@ def getcategory(request):
     # we don't jsonify it as it is already in JSON format!
     print "hello"
     return HttpResponse(response, content_type='application/json')
+
+def getpage(request):
+    url = "http://api.eventful.com/json/events/search?app_key=29hhnm59QqVdfQWf&category=" +category+ "&location=" +location+ "&date=ThisWeek&page_size=20"
+    redirec(index)
+
+    
